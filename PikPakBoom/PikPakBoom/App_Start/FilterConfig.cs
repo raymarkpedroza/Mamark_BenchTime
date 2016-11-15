084539c63a84c99a7ddbb5f15ad04a81a5e3421f
raymarkpedroza <raymark.pedroza@pointwest.com.ph>
2016-11-15 15:39:23 +0800

    Commit RockPaperScissors

---
+++ PikPakBoom/PikPakBoom/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PikPakBoom
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PikPakBoom/PikPakBoom/Controllers/PikPakBoomController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PikPakBoom.Controllers
+{
+    public class PikPakBoomController : Controller
+    {
+        // GET: PikPakBoom
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
